25ed7e5d71b78feaf54bb7c6eecf24204831b27a
Vivaan Goyal <vivaangoyal2008@gmail.com>
2026-08-25 15:16:47 +0530

    Add files via upload

---
+++ Integer Interchange.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n , reversed = 0, remainder;
+    printf("Enter an Integer:");
+    scanf("%d",&n);
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+    printf("Reversed Number: %d\n", reversed);
+    return 0;
+}
