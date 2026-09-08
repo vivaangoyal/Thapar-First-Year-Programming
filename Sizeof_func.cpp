98d77e0c45e7e4b638a90b6461f12902f12ad1a6
Vivaan Goyal <vivaangoyal2008@gmail.com>
2026-09-08 15:19:23 +0530

    Add files via upload

---
+++ Sizeof_func.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of char: %d byte\n", (int)sizeof(char));
+    printf("Size of int: %d bytes\n", (int)sizeof(int));
+    printf("Size of long int: %d bytes\n", (int)sizeof(long int));
+    printf("Size of float: %d bytes\n", (int)sizeof(float));
+    printf("Size of double: %d bytes\n", (int)sizeof(double));
+
+    return 0;
+}
+
